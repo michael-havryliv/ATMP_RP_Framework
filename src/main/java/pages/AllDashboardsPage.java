c5c2db77367c85c99f2b6b4135ddca5e02a1257f
Mykhailo_Havryliv <mykhailo_havryliv@epam.com>
2022-10-03 00:40:48 +0300

    Added checkAddNewDashboardsTest

--- src/main/java/pages/AllDashboardsPage.java
+++ src/main/java/pages/AllDashboardsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -12,6 +13,10 @@ public class AllDashboardsPage extends BasePage{
     public SelenideElement dashboardsButton = $(byXpath("//span[span[contains(text(),'Dashboards')]]"));
 
     public SelenideElement addNewDashboardButton = $(byText("Add New Dashboard"));
+
+    public SelenideElement enterDashboardNameInput = $(By.xpath("//input[@placeholder='Enter dashboard name']"));
+
+    public SelenideElement addButton = $(By.xpath("//button[text()='Add']"));
 
     public AllDashboardsPage(WebDriver driver) {
         super(driver);
--- src/test/java/ui/AllDashboardsTests.java
+++ src/test/java/ui/AllDashboardsTests.java
@@ -1,16 +1,24 @@
 package ui;
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 import pages.AllDashboardsPage;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.page;
 
 public class AllDashboardsTests extends BaseTest{
 
+    private static final String DASHBOARD_NAME = "default_name";
+
     @Test
     public void checkAddNewDashboards(){
         getAllDashboardsPage().addNewDashboardButton.click();
+        getAllDashboardsPage().enterDashboardNameInput.sendKeys(DASHBOARD_NAME);
+        getAllDashboardsPage().addButton.click();
+        getAllDashboardsPage().dashboardsButton.click();
+        $(byText(DASHBOARD_NAME)).should(Condition.exist);
     }
 
 }
